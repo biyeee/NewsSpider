81d34b5bfa359e9e28b9eb796bad39f6a950588d
biyeee <1477878823@qq.com>
2018-09-05 17:27:16 +0800

    更新

--- NewsSpider/spiders/news.py
+++ NewsSpider/spiders/news.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy import Request
+import re
+from NewsSpider.items import NewsspiderItem
 
 class NewSpider(scrapy.Spider):
     name = 'news'
     allowed_domains = ['news.sina.com.cn']
     start_urls = ['https://news.sina.com.cn/']
+    sub = [['video'], ['news'], ['finance'], ['sports'], ['ent'], ['auto'], ['fashion'], ['edu'], ['travel'], ['games'],
+           ['tech']]
 
     def parse(self, response):
         URL1 = response.xpath('//div[@class="main-nav"]/div/ul/li[1]/a/@href').extract()
-        Title = response.xpath('//div[@class="main-nav"]/div/ul/li/a/b/text()').extract()
         print(URL1)
-        print(Title)
+        # Title = response.xpath('//div[@class="main-nav"]/div/ul/li/a/b/text()').extract()
+        for url in URL1:
+            yield Request(url, callback=self.parseList, dont_filter=True)
+
+    def parseList(self, response):
+        result = re.findall(r'//(.*?).sina.com.cn', response.url)
+        if result in self.sub:
+            print(result)
